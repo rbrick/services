dca7a10311122774202d9d0bbf6fa55921a5c259
rbrick <rbrickproductions123@gmail.com>
2016-01-25 22:01:53 -0600

    go fmt yourself

--- main.go
+++ main.go
@@ -1,77 +1,77 @@
 package main
 
 import (
-    "math/rand"
-    "net/http"
-    "time"
-    "github.com/go-martini/martini"
-    "os"
-    "path/filepath"
-    "errors"
+	"errors"
+	"github.com/go-martini/martini"
+	"math/rand"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 )
 
 var (
-    CHARS = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
-    CHARS_LENGTH = len(CHARS)
+	CHARS        = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+	CHARS_LENGTH = len(CHARS)
 )
 
 const BASE_URL = "http://rbrickis.me/"
 
 func main() {
-    rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
 
-    m := martini.Classic()
+	m := martini.Classic()
 
-    // Handles uploading of images
-    m.Post("/upload_image", func(response http.ResponseWriter, request *http.Request) {
-        imgName := saveImage(request)
-        response.Write([]byte(BASE_URL + "i/" + imgName))
-    })
+	// Handles uploading of images
+	m.Post("/upload_image", func(response http.ResponseWriter, request *http.Request) {
+		imgName := saveImage(request)
+		response.Write([]byte(BASE_URL + "i/" + imgName))
+	})
 
-    m.Get("/i/:id", func(response http.ResponseWriter, request *http.Request, params martini.Params) {
-        id := params["id"]
-        fileName := "image/" + id + ".png"
+	m.Get("/i/:id", func(response http.ResponseWriter, request *http.Request, params martini.Params) {
+		id := params["id"]
+		fileName := "image/" + id + ".png"
 
-        if _, err := os.Open(fileName); os.IsNotExist(err) {
-            response.WriteHeader(404)
-            http.ServeFile(response, request, "404.html")
-            return
-        }
+		if _, err := os.Open(fileName); os.IsNotExist(err) {
+			response.WriteHeader(404)
+			http.ServeFile(response, request, "404.html")
+			return
+		}
 
-        response.Header().Set("Content-Type", "image/png")
-        http.ServeFile(response, request, fileName)
-    })
+		response.Header().Set("Content-Type", "image/png")
+		http.ServeFile(response, request, fileName)
+	})
 
-    m.NotFound(func(response http.ResponseWriter, request *http.Request) {
-        http.ServeFile(response, request, "404.html")
-    })
+	m.NotFound(func(response http.ResponseWriter, request *http.Request) {
+		http.ServeFile(response, request, "404.html")
+	})
 
-    m.RunOnAddr(":80")
+	m.RunOnAddr(":80")
 }
 
 func findFile(dir, fileName string) (*os.File, error) {
-    if _, err := os.Stat(dir); os.IsNotExist(err) {
-        return nil, errors.New("Provided directory does not exist!")
-    }
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		return nil, errors.New("Provided directory does not exist!")
+	}
 
-    var found *os.File
+	var found *os.File
 
-    visit := func(path string, info os.FileInfo, err error) error {
-        if info.Name() == fileName {
-            found, _ = os.Open(path)
-        }
-        return nil
-    }
+	visit := func(path string, info os.FileInfo, err error) error {
+		if info.Name() == fileName {
+			found, _ = os.Open(path)
+		}
+		return nil
+	}
 
-    filepath.Walk(dir, visit)
-    return found, nil
+	filepath.Walk(dir, visit)
+	return found, nil
 }
 
 // Generate a pseudo-random string of x length
 func generateRandomString(x int) string {
-    result := make([]rune, x)
-    for i := range result {
-        result[i] = CHARS[rand.Intn(CHARS_LENGTH)]
-    }
-    return string(result)
+	result := make([]rune, x)
+	for i := range result {
+		result[i] = CHARS[rand.Intn(CHARS_LENGTH)]
+	}
+	return string(result)
 }
